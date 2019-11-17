999ef4fb2cd9badd5dc8e87452d4e0c16025f2ae
Prasanna Kumar <prassee@prassee.me>
2019-11-17 09:09:31 +0530

    added more entries

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 mod store;
 use store::KeyValeRepo;
-
+// [TODO] -
 fn main() {
     let mut kv_store = KeyValeRepo::new();
     let k = format!("key");
     kv_store.add(k, format!("value"));
     println!("{}", kv_store.get(format!("key")));
     kv_store.del(format!("key"));
+    kv_store.add(format!(""), format!(""));
 }
